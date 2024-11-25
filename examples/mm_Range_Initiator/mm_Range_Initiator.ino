a1e7b55358deedee0434fbb677a3b60f5c57b8bc
hyojun01 <joon_1234@kookmin.ac.kr>
2024-11-25 12:35:04 +0900

    mm Range file setup

---
+++ examples/mm_Range_Initiator/mm_Range_Initiator.ino
@@ -0,0 +1,143 @@
+#include <DW1000Jang.hpp>
+#include <DW1000JangUtils.hpp>
+#include <DW1000JangTime.hpp>
+#include <DW1000JangConstants.hpp>
+#include <DW1000JangRanging.hpp>
+#include <DW1000JangRTLS.hpp>
+
+// connection pins
+const uint8_t PIN_RST = 7; // reset pin
+const uint8_t PIN_IRQ = 2; // irq pin
+const uint8_t PIN_SS = 10; // spi select pin
+
+// messages used in the ranging protocol
+// TODO replace by enum
+#define POLL 0
+#define POLL_ACK 1
+#define RANGE 2
+#define RANGE_REPORT 3
+#define RANGE_FAILED 255
+
+
+
+// message flow state
+volatile byte expectedMsgId = POLL_ACK;
+// message sent/received state
+volatile boolean sentAck = false;
+volatile boolean receivedAck = false;
+// timestamps to remember
+uint64_t timePollSent;
+uint64_t timePollAckReceived;
+uint64_t timeRangeSent;
+// data buffer
+#define LEN_DATA 18
+byte data[LEN_DATA];
+// watchdog and reset period
+uint32_t lastActivity;
+uint32_t resetPeriod = 250;
+
+uint16_t replyDelayTimeUS = 3000;
+
+device_configuration_t DEFAULT_CONFIG = {
+    false,
+    true,
+    true,
+    true,
+    false,
+    SFDMode::STANDARD_SFD,
+    Channel::CHANNEL_3,
+    DataRate::RATE_6800KBPS,
+    PulseFrequency::FREQ_64MHZ,
+    PreambleLength::LEN_128,
+    PreambleCode::CODE_10
+};
+
+frame_filtering_configuration_t TAG_FRAME_FILTER_CONFIG = {
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+};
+
+void setup() {
+    // DEBUG monitoring
+    Serial.begin(1000000);
+    Serial.println(F("### DW1000Jang-arduino-ranging-initiator-2 ###"));
+    // initialize the driver
+    DW1000Jang::initialize(PIN_SS, PIN_IRQ, PIN_RST);
+    Serial.println("DW1000Jang initialized ...");
+    // general configuration
+    DW1000Jang::applyConfiguration(DEFAULT_CONFIG);
+	DW1000Jang::enableFrameFiltering(TAG_FRAME_FILTER_CONFIG);
+
+    DW1000Jang::setNetworkId(10);
+    DW1000Jang::setDeviceAddress(4);
+    
+    DW1000Jang::setAntennaDelay(16436);
+    DW1000Jang::setPreambleDetectionTimeout(64);
+    DW1000Jang::setSfdDetectionTimeout(273);
+    DW1000Jang::setReceiveFrameWaitTimeoutPeriod(4000);
+}
+
+byte target_anchor[4][2] = {{0x05, 0x00},
+                            {0x06, 0x00},
+                            {0x07, 0x00},
+                            {0x08, 0x00}};
+
+uint16_t ANCHOR_INDEX = 0;
+const uint16_t CYCLE_DELAY = 1000;
+const uint16_t RECEIVE_MODE_DELAY = 1550;
+const uint16_t RESP_FINAL_DELAY = 1500;
+const uint16_t FINAL_POST_DELAY = 1700;
+
+void loop() {
+    DW1000JangRTLS::transmitPoll_v2(target_anchor[ANCHOR_INDEX]);
+    if(!DW1000JangRTLS::waitForNextRangingStep_v2(RECEIVE_MODE_DELAY)) {
+        return;
+    } 
+    else 
+    {
+        size_t cont_len = DW1000Jang::getReceivedDataLength();
+        byte cont_recv[cont_len];
+        DW1000Jang::getReceivedData(cont_recv, cont_len);
+
+        if (cont_len > 10 && cont_recv[9] == ACTIVITY_CONTROL && cont_recv[10] == RANGING_CONTINUE) 
+        {
+            /* Received Response to poll */
+            uint64_t timePollTransmitted = DW1000Jang::getTransmitTimestamp();
+            uint64_t timeResponseReceived = DW1000Jang::getReceiveTimestamp();
+            double phaseResponse = DW1000Jang::getReceivedPhase(); //Response Message를 받고 위상을 얻는 작업
+
+            DW1000JangRTLS::transmitFinalMessage_v3(
+                &cont_recv[7], 
+                RESP_FINAL_DELAY, 
+                timePollTransmitted, // Poll transmit time
+                timeResponseReceived,  // Response to poll receive time
+                phaseResponse //위상 정보를 packet에 담기
+            ); // Final Message를 보내는 시점을 현재 timestamp 시점을 기준으로 함.
+            DW1000JangRTLS::waitForTransmission();
+
+            DW1000JangRTLS::transmitPostFinalMessage(
+                &cont_recv[7],
+                FINAL_POST_DELAY
+            ); //PostFinal Message를 보내는 부분, PostFinal Message를 보내는 시점을 Final Message 송신 시점을 기준으로 함.
+            DW1000JangRTLS::waitForTransmission();
+
+            ANCHOR_INDEX = ANCHOR_INDEX + 1;
+
+            if (ANCHOR_INDEX == 4) {
+                ANCHOR_INDEX = 0;
+            }
+
+            return;
+        } 
+        else 
+        {
+            return;
+        } 
+    }
+}
---
+++ examples/mm_Range_Responder_05/mm_Range_Responder_05.ino
@@ -0,0 +1,184 @@
+#include <DW1000Jang.hpp>
+#include <DW1000JangUtils.hpp>
+#include <DW1000JangTime.hpp>
+#include <DW1000JangConstants.hpp>
+#include <DW1000JangRanging.hpp>
+#include <DW1000JangRTLS.hpp>
+
+// connection pins
+const uint8_t PIN_RST = 7; // reset pin
+const uint8_t PIN_IRQ = 2; // irq pin
+const uint8_t PIN_SS = 10; // spi select pin
+
+// timestamps to remember
+uint64_t timePollSent;
+uint64_t timePollReceived;
+uint64_t timePollAckSent;
+uint64_t timePollAckReceived;
+uint64_t timeRangeSent;
+uint64_t timeRangeReceived;
+
+uint64_t timeComputedRange;
+// last computed range/time
+// data buffer
+#define LEN_DATA 18
+byte data[LEN_DATA];
+// watchdog and reset period
+uint32_t lastActivity;
+uint32_t resetPeriod = 250;
+// reply times (same on both sides for symm. ranging)
+uint16_t replyDelayTimeUS = 3000;
+// ranging counter (per second)
+uint16_t successRangingCount = 0;
+uint32_t rangingCountPeriod = 0;
+float samplingRate = 0;
+
+device_configuration_t DEFAULT_CONFIG = {
+    false,
+    true,
+    true,
+    true,
+    false,
+    SFDMode::STANDARD_SFD,
+    Channel::CHANNEL_3,
+    DataRate::RATE_6800KBPS,
+    PulseFrequency::FREQ_64MHZ,
+    PreambleLength::LEN_128,
+    PreambleCode::CODE_10
+};
+
+frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false /* This allows blink frames */
+};
+
+void setup() {
+    // DEBUG monitoring
+    Serial.begin(1000000);
+    delay(1000);
+    // initialize the driver
+    DW1000Jang::initialize(PIN_SS, PIN_IRQ, PIN_RST);
+    // general configuration
+    DW1000Jang::applyConfiguration(DEFAULT_CONFIG);
+    DW1000Jang::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
+
+    DW1000Jang::setPreambleDetectionTimeout(64);
+    DW1000Jang::setSfdDetectionTimeout(273);
+    DW1000Jang::setReceiveFrameWaitTimeoutPeriod(8000);
+
+    DW1000Jang::setDeviceAddress(5);
+    DW1000Jang::setNetworkId(10);
+   
+    DW1000Jang::setAntennaDelay(16436);
+}
+
+const uint16_t RECEIVE_MODE_DELAY = 1550;
+const uint16_t POLL_RESP_DELAY = 1700;
+
+const double LIGHT_VELOCITY = 3 * 10e8;
+const double FREQ_CH3 = 2 * (4492.8 * 10e6);
+const double LAMBDA = LIGHT_VELOCITY / FREQ_CH3;
+
+void loop() {
+
+    double dist_twr;
+    if(!DW1000JangRTLS::receiveFrame()) {
+        return;
+    }
+    else 
+    {
+        size_t poll_len = DW1000Jang::getReceivedDataLength();
+        byte poll_data[poll_len];
+        DW1000Jang::getReceivedData(poll_data, poll_len);
+
+        if(poll_len > 9 && poll_data[9] == RANGING_TAG_POLL) 
+        {
+            uint64_t timePollReceived = DW1000Jang::getReceiveTimestamp();
+            double phasePoll = DW1000Jang::getReceivedPhase(); //Poll Message를 받고 위상을 얻는 작업
+
+            DW1000JangRTLS::transmitResponseToPoll_v3(&poll_data[7], POLL_RESP_DELAY); //기본 코드와 동일한 함수 사용, Response Message를 보내는 시점을 Poll Message 수신 시점을 기준으로 함.
+            DW1000JangRTLS::waitForTransmission();
+            uint64_t timeResponseToPoll = DW1000Jang::getTransmitTimestamp();
+
+            if(!DW1000JangRTLS::receiveFrame_v2(RECEIVE_MODE_DELAY)) {
+                return;
+            }
+            else 
+            {
+                size_t rfinal_len = DW1000Jang::getReceivedDataLength();
+                byte rfinal_data[rfinal_len];
+                DW1000Jang::getReceivedData(rfinal_data, rfinal_len);
+
+                if(rfinal_len > 22 && rfinal_data[9] == RANGING_TAG_FINAL_RESPONSE_EMBEDDED) {
+                    uint64_t timeFinalMessageReceive = DW1000Jang::getReceiveTimestamp();
+                    double phaseFinal = DW1000Jang::getReceivedPhase(); //Final Message를 받고 위상을 얻는 작업
+                    // Final Message를 받은 후 packet에 존재하는 정보를 먼저 변수에 저장
+                    double phaseResponse = static_cast<double>(DW1000JangUtils::bytesAsValue(&rfinal_data[22], 4) / 1000.0);
+                    uint64_t timePollSent = DW1000JangUtils::bytesAsValue(&rfinal_data[10], 4);
+                    uint64_t timeResponseToPollReceived = DW1000JangUtils::bytesAsValue(&rfinal_data[14], 4);
+                    uint64_t timeFinalMessageSent = DW1000JangUtils::bytesAsValue(&rfinal_data[18], 4);
+
+                    // PostFinal Message를 받는 부분 추가
+                    if (!DW1000JangRTLS::receiveFrame_v3(RECEIVE_MODE_DELAY)) {
+                      return;
+                    }
+                    else
+                    {
+                      size_t rpostfinal_len = DW1000Jang::getReceivedDataLength();
+                      byte rpostfinal_data[rpostfinal_len];
+                      DW1000Jang::getReceivedData(rpostfinal_data, rpostfinal_len);
+
+                      if (rpostfinal_len > 9 && rpostfinal_data[9] == RANGING_TAG_POST_FINAL_RESPONSE_EMBEDDED) {
+                        double phasePostFinal = DW1000Jang::getReceivedPhase(); //PostFinal Message를 받고 위상을 얻는 작업
+
+                        dist_twr = DW1000JangRanging::computeRangeAsymmetric(
+                            timePollSent, // Poll send time
+                            timePollReceived, 
+                            timeResponseToPoll, // Response to poll sent time
+                            timeResponseToPollReceived, // Response to Poll Received
+                            timeFinalMessageSent, // Final Message send time
+                            timeFinalMessageReceive // Final message receive time
+                        ); // timestamp를 저장한 변수들을 가지고 거리를 계산하는 작업
+
+                        if (dist_twr <= 0) {
+                            dist_twr = 0.000001;
+                        }
+
+                        double sumPollResp = fmod((phasePoll + phaseResponse), 2 * PI);
+                        if (sumPollResp < 0) {
+                          sumPollResp += 2 * PI;
+                        }
+
+                        double diffFinalPost = fmod((phaseFinal - phasePostFinal), 2 * PI);
+                        if (diffFinalPost < 0) {
+                          diffFinalPost += 2 * PI;
+                        }
+                        
+                        double recPhase = fmod(sumPollResp - diffFinalPost, 2 * PI);
+                        if (recPhase < 0) {
+                            recPhase += 2 * PI;
+                        }
+                        
+                        // int N = floor(dist_twr / LAMBDA);
+                        // double dist = ((recPhase / (2 * PI)) + N) * LAMBDA;
+										
+                        // Serial.println(dist);
+
+                        Serial.print(recPhase);
+                        Serial.print("|");
+                        Serial.println(dist_twr);
+                      }
+                    }
+                }
+            }
+        }
+    }
+}
+    
+
